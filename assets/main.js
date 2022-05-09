6dd4c14b1359619fc8f15c0f3399d9251bd0cd80
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-05-09 14:51:46 -0400

    adding major updates to main.js

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,14 @@
+
 function WeatherController() {
 
   this.apikey = "31f3c7fba0e24b5ad83d1dc92397b585"
   this.myunits = "imperial"
   this.currentData = null
-  this.forecastData = null
+  this.oneCallData = null
   
-  this.displayForecast = () => {
-    console.log(this.forecastData)
-    if (this.forecastData != null) {
+  this.displayOnecall = () => {
+    console.log(this.oneCallData)
+    if (this.oneCallData != null) {
       console.log('have data');
     }
   }
@@ -27,98 +28,102 @@ function WeatherController() {
       this.displayCurrent();
       var lat = data.coord.lat;
       var lon = data.coord.lon;
+      let name = data.name;
+      let time = data.dt;
+      console.log(name);
+      console.log(time);
       console.log(lat);
       console.log(lon);
-      this.getForecastData(lat, lon);
-
+      this.getOnecallData(lat, lon);
+      
     }
-  
-  this.getForecastData = async (lat, lon) => {
+  this.getOnecallData = async (lat, lon) => {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${this.apikey}&units=${this.myunits}`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${this.apikey}&units=${this.myunits}`
     );
     const data = await response.json();
     console.log(data);
-    this.forecastData = data
-    this.displayForecast();
+    this.oneCallData = data;
+    this.displayOnecall();
+    let cityList=document.getElementById('cityList')
+    let content= document.getElementById('content');
+    let content2=document.getElementById("content2");
+   
+    content.innerHTML = 
+      `<section class ="weatherCards">
+      <div class="card row-2">
+      <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png"></img></div>
+          <div>${city} date: ${data.daily[0].dt}</div>
+          <div>Temp: ${data.daily[0].temp.day} &#176</div>
+          <div>Wind: ${data.daily[0].wind_speed} MPH</div>
+          <div>Humidity: ${data.daily[0].humidity} %</div>
+          <div>UV Index: ${data.daily[0].uvi}</div>
+        </div>
+      </div>
+      </section>
+      `;
+      content2.innerHTML = ` 
+    <section class ="weatherCards">
+    <div class="card row-2">
+    <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png"></img></div>
+        <div>${city} date: ${data.daily[0].dt}</div>
+        <div>Temp: ${data.daily[0].temp.day} &#176</div>
+        <div>Wind: ${data.daily[0].wind_speed} MPH</div>
+        <div>Humidity: ${data.daily[0].humidity} %</div>
+       
+      </div>
+    </div>
+    </section>
+    <section class ="weatherCards">
+    <div class="card row-2">
+    <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}@2x.png"></img></div>
+        <div>${city} date: ${data.daily[1].dt}</div>
+        <div>Temp: ${data.daily[1].temp.day} &#176</div>
+        <div>Wind: ${data.daily[1].wind_speed} MPH</div>
+        <div>Humidity: ${data.daily[1].humidity} %</div>
+      </div>
+    </div>
+    </section>
+    <section class ="weatherCards">
+    <div class="card row-2">
+    <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}@2x.png"></img></div>
+        <div>${city} date: ${data.daily[2].dt}</div>
+        <div>Temp: ${data.daily[2].temp.day} &#176</div>
+        <div>Wind: ${data.daily[2].wind_speed} MPH</div>
+        <div>Humidity: ${data.daily[2].humidity} %</div>
+      </div>
+    </div>
+    </section>
+    <section class ="weatherCards">
+    <div class="card row-2">
+    <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[3].weather[0].icon}@2x.png"></img></div>
+        <div>${city} date: ${data.daily[3].dt}</div>
+        <div>Temp: ${data.daily[3].temp.day} &#176</div>
+        <div>Wind: ${data.daily[3].wind_speed} MPH</div>
+        <div>Humidity: ${data.daily[3].humidity} %</div>
+      </div>
+    </div>
+    </section>
+    <section class ="weatherCards">
+    <div class="card myrow row-2">
+    <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[4].weather[0].icon}@2x.png"></img></div>
+        <div>${city} date: ${data.daily[4].dt}</div>
+        <div>Temp: ${data.daily[4].temp.day} &#176</div>
+        <div>Wind: ${data.daily[4].wind_speed} MPH</div>
+        <div>Humidity: ${data.daily[4].humidity} %</div>
+      </div>
+    </div>
+    </section>
+    `;
+  cityList.innerHTML = `<div>${city}</div>`
+    
+};
 
-  }
 }
 let submit = document.getElementById("submit");
 let controller = new WeatherController();
 
-
-
 submit.addEventListener('click', () => {
   this.city = $("#searchUser").val();
-
   controller.getCurrentData(city);
 })
-// var myKey = "31f3c7fba0e24b5ad83d1dc92397b585";
-
-//  var content2= document.getElementById("content2");
-
-// function test2(){   
-  
-//   var currentVal = search.value;
-//       fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${currentVal}&appid=${myKey}&units=imperial`)
-//         .then(function(response) {
-//             return response.json();
-//         })
-//         .then(function(data) {
-//             for(i=0;i<5;i++){
-// console.log(data)
-//               this.content2.innerHTML=` 
-//               <section class ="weatherCards">
-//               <div class="card col-2">
-//                 <div class=" col card-body"><img src= "https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png"></img>${data.list[i].dt_txt}</div>
-//                   <div>Temp: ${data.list[i].main.temp} &#176</div>
-//                   <div>Wind: ${data.list[i].wind.speed} MPH</div>
-//                   <div>Humidity: ${data.list[i].main.humidity} %</div>
-                  
-//                 </div>
-//               </div>
-//               <div class="card col-2">
-//                 <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.list[i+1].weather[0].icon}@2x.png"></img>${data.list[i+1].dt_txt}</div>
-//                   <div>Temp: ${data.list[i+1].main.temp} &#176</div>
-//                   <div>Wind: ${data.list[i+1].wind.speed} MPH</div>
-//                   <div>Humidity:${data.list[i+1].main.humidity} %</div>
-                  
-//                 </div>
-//               </div>
-//               <div class="card col-2">
-//                 <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.list[i+2].weather[0].icon}@2x.png"></img>${data.list[i+2].dt_txt}</div>
-//                   <div>Temp: ${data.list[i+2].main.temp} &#176</div>
-//                   <div>Wind: ${data.list[i+2].wind.speed} MPH</div>
-//                   <div>Humidity: ${data.list[i+2].main.humidity} %</div>
-                  
-//                 </div>
-//               </div>
-//               <div class="card col-2">
-//                 <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.list[i+3].weather[0].icon}@2x.png"></img>${data.list[i+3].dt_txt}</div>
-//                   <div>Temp: ${data.list[i+3].main.temp} &#176</div>
-//                   <div>Wind: ${data.list[i+3].wind.speed} MPH</div>
-//                   <div>Humidity: ${data.list[i+3].main.humidity}%</div>
-                  
-//                 </div>
-//               </div>
-//               <div class="card col-2">
-//                 <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.list[i+4].weather[0].icon}@2x.png"></img>${data.list[i+4].dt_txt}</div>
-//                   <div>Temp: ${data.list[i+4].main.temp} &#176</div>
-//                   <div>Wind: ${data.list[i+4].wind.speed} MPH</div>
-//                   <div>Humidity: ${data.list[i+4].main.humidity} %</div>
-                  
-//                 </div>
-//                 <div class="card col-2">
-//                 <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.list[i+5].weather[0].icon}@2x.png"></img>${data.list[i+5].dt_txt}</div>
-//                   <div>Temp: ${data.list[i+5].main.temp}&#176</div>
-//                   <div>Wind: ${data.list[i+5].wind.speed} MPH</div>
-//                   <div>Humidity: ${data.list[i+5].main.humidity} %</div>
-                  
-//                 </div>
-//               </div>
-//               </section>
-//               `
-//             }
-//         })
-// };
