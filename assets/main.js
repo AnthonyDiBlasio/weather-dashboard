79cad6977c34fdf48d359a1ece1f1cc96f8796e2
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-05-10 12:29:42 -0400

    adding a card and for loop not working

--- assets/main.js
+++ assets/main.js
@@ -15,25 +15,25 @@ function WeatherController() {
       console.log('have data');
     }
   }
-    this.getCurrentData = async (city) => {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apikey}&units=${this.myunits}`
-      );
-      const data = await response.json();
-      console.log(data);
-      this.currentData = data
-      this.displayCurrent();
-      var lat = data.coord.lat;
-      var lon = data.coord.lon;
-      let name = data.name;
-      let time = data.dt;
-      console.log(name);
-      console.log(time);
-      console.log(lat);
-      console.log(lon);
-      this.getOnecallData(lat, lon);
-      
-    }
+  this.getCurrentData = async (city) => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apikey}&units=${this.myunits}`
+    );
+    const data = await response.json();
+    console.log(data);
+    this.currentData = data
+    this.displayCurrent();
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+    let name = data.name;
+    let time = data.dt;
+    console.log(name);
+    console.log(time);
+    console.log(lat);
+    console.log(lon);
+    this.getOnecallData(lat, lon);
+
+  }
   this.getOnecallData = async (lat, lon) => {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${this.apikey}&units=${this.myunits}`
@@ -42,13 +42,13 @@ function WeatherController() {
     console.log(data);
     this.oneCallData = data;
     this.displayOnecall();
-    let cityList=document.getElementById('cityList')
-    let content= document.getElementById('content');
-    let content2=document.getElementById("content2");
-   
-    content.innerHTML = 
+    let cityList = document.getElementById('cityList')
+    let content = document.getElementById('content');
+
+
+    content.innerHTML =
       `<section class ="weatherCards">
-      <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png"></img>
+        <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png"></img>
           <div>${city} date: ${data.daily[0].dt}</div>
           <div>Temp: ${data.daily[0].temp.day} &#176</div>
           <div>Wind: ${data.daily[0].wind_speed} MPH</div>
@@ -58,55 +58,51 @@ function WeatherController() {
       </div>
       </section>
       `;
-      content2.innerHTML = ` 
-    <section class =" card weatherCards">
-    <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png"></img>
-        <div>${city} date: ${data.daily[0].dt}</div>
-        <div>Temp: ${data.daily[0].temp.day} &#176</div>
-        <div>Wind: ${data.daily[0].wind_speed} MPH</div>
-        <div>Humidity: ${data.daily[0].humidity} %</div>
-      </div>
-    </div>
-    </section>
-    <section class =" card weatherCards">
-    <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}@2x.png"></img>
-        <div>${city} date: ${data.daily[1].dt}</div>
-        <div>Temp: ${data.daily[1].temp.day} &#176</div>
-        <div>Wind: ${data.daily[1].wind_speed} MPH</div>
-        <div>Humidity: ${data.daily[1].humidity} %</div>
-      </div>
-    </div>
-    </section>
-    <section class =" card weatherCards">
-    <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}@2x.png"></img>
-        <div>${city} date: ${data.daily[2].dt}</div>
-        <div>Temp: ${data.daily[2].temp.day} &#176</div>
-        <div>Wind: ${data.daily[2].wind_speed} MPH</div>
-        <div>Humidity: ${data.daily[2].humidity} %</div>
-      </div>
-    </div>
-    </section>
-    <section class =" card weatherCards">
-    <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[3].weather[0].icon}@2x.png"></img>
-        <div>${city} date: ${data.daily[3].dt}</div>
-        <div>Temp: ${data.daily[3].temp.day} &#176</div>
-        <div>Wind: ${data.daily[3].wind_speed} MPH</div>
-        <div>Humidity: ${data.daily[3].humidity} %</div>
-      </div>
-    </div>
-    </section>
-    <section class =" card weatherCards">
-    <div class="card-body"><img src= "https://openweathermap.org/img/wn/${data.daily[4].weather[0].icon}@2x.png"></img>
-        <div>${city} date: ${data.daily[4].dt}</div>
-        <div>Temp: ${data.daily[4].temp.day} &#176</div>
-        <div>Wind: ${data.daily[4].wind_speed} MPH</div>
-        <div>Humidity: ${data.daily[4].humidity} %</div>
-      </div>
-    </div>
-    </section>
-    `;
- 
-};
+   let content2 = document.getElementById("content2");
+    for (let i = 0;i<data.daily.length;i++) {
+
+      content2.innerHTML=` 
+      
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-md-8 col-lg-6 col-xl-4">
+      
+              <div class="card" style="color: #4B515D; border-radius: 35px;">
+                <div class="card-body p-4">
+      
+                  <div class="d-flex">
+                    <h6 class="flex-grow-1">${city}</h6>
+                    <h6>${data.daily[i].dt}</h6>
+                  </div>
+      
+                  <div class="d-flex flex-column text-center mt-5 mb-4">
+                    <h6 class="display-4 mb-0 font-weight-bold" style="color: #1C2331;"> ${data.daily[i].temp.day}°F </h6>
+                  </div>
+      
+                  <div class="d-flex align-items-center">
+                    <div class="flex-grow-1" style="font-size: 1rem;">
+                      <div><i class="fas fa-wind fa-fw" style="color: #868B94;"></i> <span class="ms-1">Wind: ${data.daily[i].wind_speed} mph
+                        </span></div>
+                      <div><i class="fas fa-tint fa-fw" style="color: #868B94;"></i> <span class="ms-1">Humidity: ${data.daily[i].humidity}% </span>
+                      </div>
+                      </div>
+                    </div>
+                    <div>
+                      <img src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png""
+                        width="100px">
+                    </div>
+                  </div>
+      
+                </div>
+              </div>
+      
+            </div>
+          </div>
+      `;
+    }
+
+
+  };
+
 
 }
 let submit = document.getElementById("submit");
